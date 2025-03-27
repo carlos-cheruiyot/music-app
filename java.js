558cbd18e968e92b6d8493d410d6ae9a21d7697c
carlos <carloskip25@gmail.com>
2025-03-27 13:15:25 +0300

    feat:adding functionality by adding event listeners

--- java.js
+++ java.js
@@ -1,1 +1,7 @@
-db
+document.addEventListener("DOMContentLoaded", () => {
+    const searchInput = document.getElementById("searchInput");
+    const searchButton = document.getElementById("searchButton");
+    const songList = document.getElementById("songList");
+    const playlistList = document.getElementById("playlistList");
+    const audioPlayer = document.getElementById("audioPlayer");
+});
